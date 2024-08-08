190d24fc242d4660fac7a5d86ad82815732fbb7f
omarghandour <alex.forget50@gmail.com>
2024-08-08 14:02:16 +0300

    past dates on deadline dates calender are disabled.

--- components/AddTask.tsx
+++ components/AddTask.tsx
@@ -221,9 +221,9 @@ const AddTask = () => {
                       mode="single"
                       selected={field.value}
                       onSelect={field.onChange}
-                      // disabled={(date) =>
-                      //   da te > new Date() || date < new Date("1900-01-01")
-                      // }
+                      disabled={(date) =>
+                        date < new Date() || date < new Date("1900-01-01")
+                      }
                       initialFocus
                     />
                   </PopoverContent>
